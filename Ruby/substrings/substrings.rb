60c10b75201fa35e992ec324f22b146358c3c6af
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-06 22:36:11 +1000

    Can check whole words against the dictionary array using include?

--- Ruby/substrings/substrings.rb
+++ Ruby/substrings/substrings.rb
@@ -1,15 +1,32 @@
 
+wordlist =      ["below","down","go","going","horn",
+                  "how","howdy","it","i","low","own",
+                  "part","partner","sit","sniffle","hello",
+                "world"]
+
 
 def substrings(string, dictionary)
 
-wordlist =      ["below","down","go","going","horn",
-                  "how","howdy","it","i","low","own",
-                  "part","partner","sit","sniffle"]
 
 
+string_array = string.split(" ")
+
+for word in string_array do
+
+if dictionary.include?(word)
+  p "#{word} is here!"
 
 
+end
+
+end
 
 
 
 end
+
+substrings("hello world", wordlist)
+
+#Alright, so I can very easily isolate whole words with include
+# but I need to figure out how I'm going to isolate the substrings
+# That is a job for tomorrow, good night for now :)
