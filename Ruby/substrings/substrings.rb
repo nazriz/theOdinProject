0e9f31c79b28879b2f7690584ed3c18bec9a88c7
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-07 20:18:16 +1000

    Made program work according to project requirements.

--- Ruby/substrings/substrings.rb
+++ Ruby/substrings/substrings.rb
@@ -10,14 +10,15 @@ def substrings(string, dictionary)
 hash = Hash.new(0) #initaite hash with a zero value, so that we can increment it
 
 string_modified = string.downcase.gsub(/[^a-z0-9\s]/i, '')
-# removes punctuation, makes the string lowercase, and splits at spaces.
+# removes punctuation, makes the string lowercase, splits into array
 
 dictionary.each do |word|
 
 if string_modified.include?(word)
-  hash[word] += 1
-
+  counter = string_modified.scan(word).count
+  hash[word] = counter
 end
+
 end
 
 p  hash
