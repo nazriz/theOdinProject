58fcd85f589d063ef478a01970acf49c4933e17b
Nazim Rizvic <nazim.rizvic@me.com>
2020-05-08 16:31:57 +1000

    Got the loop and nested loop ready to manipulate data

--- Ruby/stock_picker/stock_picker.rb
+++ Ruby/stock_picker/stock_picker.rb
@@ -3,18 +3,47 @@ stonks = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(stocks)
 
-
 stocks.each do |stock|
 
-  if stocks.index(stock) == 0
-    buy_price = stock.to_s
-    p buy_price
-  end
+
+i = stocks.index(stock)
+while i <= stocks.length
+
+  p i
+
+i += 1
+end
+p "END OF LOOP"
+
+end
 
 
 
 end
 
-end
+
 
 stock_picker(stonks)
+
+
+
+#Alright, well, I know what I want to do. I just need to implement it.
+
+# buy_price = ""
+# sell_price = ""
+#
+# stocks.each do |stock|
+#
+#   if stocks.index(stock) == 0
+#     buy_price = stock
+#     p buy_price
+#   end
+#
+# if buy_price > stock
+#   buy_price = stock
+#   p buy_price
+# end
+#
+# end
+#
+# end
